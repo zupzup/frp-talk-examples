71fab5727ed310bf265519b0b86cf0e11f19fffd
Mario Zupan <mzupanmz@gmail.com>
2014-11-13 16:06:17 +0100

    add interval example

--- examples.js
+++ examples.js
@@ -10,7 +10,14 @@ function logContent(data) {
 $(function() {
     var data  = [1, 2, 3, 4];
     var dataStream = Rx.Observable.from(data);
-    var range = Rx.Observable.range(1, 4);
+
+    var range = Rx.Observable.range(1, 4); //this is the same thing
+
+    var intervalStream = Rx.Observable.interval(500);
+
+    var dataIntervalStream = Rx.Observable.zip(range, intervalStream, function(value, tick) {
+        return value;
+    });
 
     dataStream.subscribe(
         function(x) {
@@ -21,6 +28,7 @@ $(function() {
             logContent('completed');
         }
     );
+
     range.subscribe(
         function(x) {
             logContent(x);
@@ -30,4 +38,20 @@ $(function() {
             logContent('completed');
         }
     );
+
+    dataIntervalStream
+    .filter(function(v) {
+        return v%2 === 0; 
+    }).map(function(v) {
+        return v + " is an even number";
+    }).subscribe(
+        function(x) {
+            logContent(x);
+        },
+        function() {},
+        function() {
+            logContent('completed');
+        }
+    );
 });
+
